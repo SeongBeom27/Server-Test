9f0260a3aaaade022abd6b244756e7498b6c6ec3
SeongBeom27 <lgh121546@gmail.com>
2021-08-22 21:11:57 +0900

    [개발 사항] main 파일로 합침
    multi thread 형태로 변경함
      - main thread는 Accept
      - 각 thread는 연결된 client와 read/write

--- Server/src/main.cc
+++ Server/src/main.cc
@@ -8,18 +8,127 @@
  * @copyright Copyright (c) 2021
  * 
  */
+// C++ header
+#include <iostream>
+#include <string>
+#include <netdb.h>
+#include <map>
 
-#include "server_sock.h"
+// C header
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/socket.h>
+#include <pthread.h>
+
+using namespace std;
+
+#define BUFSIZE 300
+
+pthread_t g_thread[1024];
+int g_clientCount = 0;
+
+struct client {
+    int index;
+    int id;
+    socklen_t len;
+    struct sockaddr_in clnt_addr;
+};
+
+map<int, struct client> m_clntMap;
+
+void error_handling(char *message);
+void LOG(char *message);
+
+void *thread_msg(void * pClient){
+    char message[BUFSIZE];
+
+    int str_len;
+    while((str_len = read(((struct client *)pClient)->id, message, BUFSIZE)) != 0)
+    {
+        // client 측에 받은 데이터를 echo 형식으로 보냄
+        write(((struct client *)pClient)->id, message, str_len);
+        
+        // write(1, inet_ntoa(clnt_addr.sin_addr), strlen(inet_ntoa(clnt_addr.sin_addr)) + 1);	
+        // write(1, " : ", 4);	
+        // write(1, message, str_len);
+
+        printf("%d : %s\n", ((struct client *)pClient)->id, message);
+    }
+
+    puts("connect exit\n");
+    return NULL;
+}
 
 int main(){
-    CSocket *pSock;
+    struct sockaddr_in serv_addr;
 
-    pSock = new CSocket();
-    pSock->Open("8080");
-    int c_sock = pSock->Accept();
-    pSock->Read(c_sock);
+    int serv_sock;
+	int addr_size, str_len, state;
+    const char _port[] = "8080";
 
-    delete(pSock);
+	pid_t pid;
+
+    // Socket Open
+	serv_sock=socket(PF_INET, SOCK_STREAM, 0);
+
+	memset(&serv_addr, 0, sizeof(serv_addr));
+	serv_addr.sin_family=AF_INET;
+	serv_addr.sin_addr.s_addr=htonl(INADDR_ANY);
+	serv_addr.sin_port=htons(atoi(_port));
+
+	if(bind(serv_sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) == -1)
+	{
+		printf("bind() error\n");
+	}
+
+	if(listen(serv_sock, 5) == -1)
+	{
+		printf("listen() error\n");
+	}
+
+    while(1){
+        struct client Client;
+
+        Client.len = sizeof(Client.clnt_addr);
+        
+        Client.id = accept(serv_sock, (struct sockaddr*) &Client.clnt_addr, &Client.len);
+        if(Client.id == -1)
+        {
+            printf("[CSocket::Accept] Client.id error occur!");
+            return Client.id;
+        }
+
+        Client.index = g_clientCount;
+
+        m_clntMap[Client.id] = Client;
+        printf("Id : %d번님이 입장하셨습니다.\n", Client.id);
+
+        pthread_create(&g_thread[g_clientCount], NULL, thread_msg, (void *) &Client);
+
+        g_clientCount++;
+    }
+    
+    for(int i = 0 ; i < g_clientCount ; i ++)
+		pthread_join(g_thread[i], NULL);
 
     return 0;
+}
+
+void error_handling(char *message)
+{
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
+
+void LOG(char *message)
+{
+    fputs(message, stdout);
+    fputc('\n', stdout);
 }
